b1b0bfcbf2849121a156c2a141a19231557ae1ad
Kasper-S-Jensen <kasperscoop@gmail.com>
2023-02-28 19:23:49 +0100

    Created player with a player controller and a camera controller

---
+++ Lection2Scripting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+   private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var v3 = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        transform.Translate(v3.normalized * (speed * Time.deltaTime));   
+
+        
+        //inefficient way
+        /*
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right*(speed*Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-(speed * Time.deltaTime), 0f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0f, 0f, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0f, 0f, -(speed * Time.deltaTime));
+        }
+        */
+
+       
+    }
+}
